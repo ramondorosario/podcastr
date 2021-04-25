3b1d161972d323e91df5e5a2c02d086d3f0a85b4
ramondorosario <ramondorosario@gmail.com>
2021-04-24 23:04:30 -0300

    feat: using ssg

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,15 @@
-export default function Home() {
-  return <h1>Hello world</h1>;
+export default function Home(props) {
+  return <span>{props.episodes}</span>;
 }
+
+export const getStaticProps = async () => {
+  const response = await fetch("http://localhost:3333/episodes");
+  const data = await response.json();
+
+  return {
+    props: {
+      episodes: JSON.stringify(data),
+    },
+    revalidate: 60 * 60 * 8,
+  };
+};
