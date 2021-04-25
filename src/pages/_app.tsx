8694939f8c045a152f2d1fcaf2339eff9fc4e579
ramondorosario <ramondorosario@gmail.com>
2021-04-24 23:02:49 -0300

    feat: importing components

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,20 @@
 import "../styles/global.scss";
 
+import { Header } from "../components/components/Header";
+import { Player } from "../components/components/Player";
+
+import s from "../styles/app.module.scss";
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <div className={s.container}>
+      <main>
+        <Header />
+        <Component {...pageProps} />
+      </main>
+      <Player />
+    </div>
+  );
 }
 
 export default MyApp;
