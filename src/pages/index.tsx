0f2d5145218cfd88e74e6a0f2675d62c22052547
ramondorosario <ramondorosario@gmail.com>
2021-04-26 22:47:15 -0300

    feat: create allEpisodes section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -60,7 +60,48 @@ export default function Home(props: IHomesProps) {
           })}
         </ul>
       </section>
-      <section className={s.allEpisodes}></section>
+      <section className={s.allEpisodes}>
+        <h2>Todos os episódios</h2>
+        <table cellSpacing={0}>
+          <thead>
+            <tr>
+              <th colSpan={2}>Podcast</th>
+              <th>Integrantes</th>
+              <th>Data</th>
+              <th>Duração</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {allEpisodes.map((episode) => {
+              return (
+                <tr key={episode.id}>
+                  <td className={s.image}>
+                    <Image
+                      width="120"
+                      height="120"
+                      objectFit="cover"
+                      src={episode.thumbnail}
+                      alt={episode.title}
+                    />
+                  </td>
+                  <td>
+                    <a href="">{episode.title}</a>
+                  </td>
+                  <td>{episode.members}</td>
+                  <td className={s.date}>{episode.published_at}</td>
+                  <td>{episode.file.duration}</td>
+                  <td>
+                    <button>
+                      <img src="/images/play-green.svg" alt="Tocar podcast" />
+                    </button>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </section>
     </div>
   );
 }
@@ -87,7 +128,7 @@ export const getStaticProps: GetStaticProps = async () => {
   });
 
   const latestEpisodes = episodes.slice(0, 2);
-  const allEpisodes = episodes.slice(2, -1);
+  const allEpisodes = episodes.slice(2);
 
   return {
     props: {
