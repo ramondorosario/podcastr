74201ccdc79e5c3f93282dd32f54d9804681e3ef
ramondorosario <ramondorosario@gmail.com>
2021-05-02 00:20:12 -0300

    feat: creating new functions and state to PlayerContext

--- src/contexts/PlayerContext.tsx
+++ src/contexts/PlayerContext.tsx
@@ -16,7 +16,10 @@ interface IEpisode {
 interface IPlayerContext {
   episodeList: IEpisode[];
   currentEpisodeIndex: number;
+  isPlaying: boolean;
   play: (episode: IEpisode) => void;
+  togglePlay: () => void;
+  setPlayingState: (state: boolean) => void;
 }
 
 export const PlayerContext = createContext({} as IPlayerContext);
@@ -24,14 +27,33 @@ export const PlayerContext = createContext({} as IPlayerContext);
 export const PlayerContextProvider: React.FC = ({ children }) => {
   const [episodeList, setEpisode] = useState<IEpisode[]>([]);
   const [currentEpisodeIndex, setCurrentEpisodeIndex] = useState<number>(0);
+  const [isPlaying, setIsPlaying] = useState<boolean>(false);
 
   const play = (episode: IEpisode) => {
     setEpisode([episode]);
     setCurrentEpisodeIndex(0);
+    setIsPlaying(true);
+  };
+
+  const togglePlay = () => {
+    setIsPlaying(!isPlaying);
+  };
+
+  const setPlayingState = (state: boolean) => {
+    setIsPlaying(state);
   };
 
   return (
-    <PlayerContext.Provider value={{ episodeList, currentEpisodeIndex, play }}>
+    <PlayerContext.Provider
+      value={{
+        episodeList,
+        currentEpisodeIndex,
+        isPlaying,
+        play,
+        togglePlay,
+        setPlayingState,
+      }}
+    >
       {children}
     </PlayerContext.Provider>
   );
